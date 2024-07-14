bf84b3cf7f2744f900eb0c9d21ec5a5938c799d2
Ricardo Silva <ricardooandree@gmail.com>
2024-07-14 13:12:48 +0100

    Initial commit

---
+++ src/main/java/com/ricardooandree/movie_review_api/MovieReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.ricardooandree.movie_review_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardooandree/movie_review_api/MovieReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardooandree.movie_review_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
