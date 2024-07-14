598afe8bbc9a4ae539f6285beaf025c24fb1e9c0
Ricardo Silva <ricardooandree@gmail.com>
2024-07-14 19:50:02 +0100

    Updates Repositories

--- src/main/java/com/ricardooandree/movie_review_api/repository/ReviewRepository.java
+++ src/main/java/com/ricardooandree/movie_review_api/repository/ReviewRepository.java
@@ -1,5 +1,23 @@
 package com.ricardooandree.movie_review_api.repository;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ricardooandree.movie_review_api.models.Review;
+import java.util.List;
+
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long>{
+
+    @Transactional(readOnly = true)
+    Review findByMovie(String movie);
+
+    @Transactional(readOnly = true)
+    List<Review> findByScore(int score);
+
+    @Transactional(readOnly = true)
+    List<Review> findByUser_Id(Long userId);
+    
 }
--- src/main/java/com/ricardooandree/movie_review_api/repository/UserRepository.java
+++ src/main/java/com/ricardooandree/movie_review_api/repository/UserRepository.java
@@ -1,5 +1,16 @@
 package com.ricardooandree.movie_review_api.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ricardooandree.movie_review_api.models.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 
 }
